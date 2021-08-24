711a692f4be7a3a5c8f96caa8e6fbdf2c0f3c1ad
cornelialundell <lundellcornelia@gmail.com>
2021-08-24 11:49:58 +0200

    landingPage

--- client/src/components/landingPage/landingPage.tsx
+++ client/src/components/landingPage/landingPage.tsx
@@ -1,5 +1,12 @@
-export const landingPage = () => {
-    return (
-        <div>hejhej</div>
-    )
-}
+import React from "react";
+import { Wrapper } from "./landingPage.style";
+import {ReactComponent as ReactLogo} from './../../img/fucion-logo.svg'
+
+export const LandingPage = () => {
+  return (
+    <Wrapper>
+      <h1>Välkomna</h1>
+      <ReactLogo/>
+    </Wrapper>
+  );
+};
