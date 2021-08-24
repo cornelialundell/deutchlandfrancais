fbd375ab77b0eb99e3ef9bfa34bc081d4edc81a7
cornelialundell <lundellcornelia@gmail.com>
2021-08-24 19:15:34 +0200

    first steps of booking

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import { LandingPage } from './components/landingPage/landingPage';
+import { LandingPage } from './components/LandingPage/landingPage'
 import { ContactPage } from './components/contactPage/contactPage';
 import { Navigation} from './components/navigation/navigation';
+import { BookingPage } from './components/bookingPage/bookingPage';
 
 function App() {
   return (
@@ -19,8 +20,8 @@ function App() {
       <ContactPage/> 
     </Route>
     <Route exact path="/booking">
-    {/* <Nav />
-      <BookingForm /> */}
+    <Navigation />
+      <BookingPage />
     </Route>
 </Switch>
   </Router>
---
+++ client/src/components/bookingPage/booking.ts
@@ -0,0 +1,8 @@
+export class Booking {
+    day?: Date;
+    guests?: number = 0;
+    time?: number;
+    name?: string;
+    email?: string;
+    phones?: string;
+}
