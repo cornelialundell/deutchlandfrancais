158bec6b55890f1a5ab8bc0a9b981b5b12e29350
Evelina Wenner <evelina.nilssonvenner@medieinstitutet.se>
2021-09-07 15:09:40 +0200

    async test

---
+++ client/src/components/adminPage/seeBookings.test.tsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { AdminPage } from "./adminPage";
+
+jest.mock("axios");
+const mockAxios = axios as jest.Mocked<typeof axios>;
+
+
+
+
+test("Can log in to admin page", async () => {
+  mockAxios.post.mockResolvedValue({ data:{} });
+  render(<AdminPage />);
+  const usernameInput = screen.getByPlaceholderText("username");
+  const passwordInput = screen.getByPlaceholderText("password");
+  fireEvent.change(usernameInput, { target: { value: "lorem" } });
+  fireEvent.change(passwordInput, { target: { value: "ipsum" } });
+
+  const SignInButton = document.querySelector("button");
+  SignInButton?.click();
+
+  await waitFor(() => {
+      const signOutButton = screen.getByText(/Logga ut/i)
+    expect(signOutButton).toBeInTheDocument();
+  });
+});
+
+// jest.mock(”axios”),
+// const mockAxios = axios as jest.Mocked<typeof axios>,
+// const mockData: IMovie[] = [{…}, {…}, …],
+
+// test(“Movies Should have correct number of movies”, () => {
+// mockAxios.get.mockResolveValues({data: mockData});
+// render(<Movies/>);
+// await waitFor(() => {
+// const movies = screen.getAllByRole(“heading”);
+// expect(movies.length).toBe(mockData.length),
+// });
+// });
