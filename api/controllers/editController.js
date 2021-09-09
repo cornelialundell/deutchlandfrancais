c49ad2c3ccb3f9c3e9a99a032690a7bdea700932
cornelialundell <lundellcornelia@gmail.com>
2021-09-09 12:34:34 +0200

    fixed checkavailability code

--- api/controllers/editController.js
+++ api/controllers/editController.js
@@ -6,48 +6,53 @@ const getBooking = async (req, res) => {
   let booking = await Booking.find({
     confirmation: confirmation,
   });
-booking = booking[0]
+  booking = booking[0];
   res.send(booking);
 };
 
 const updateBooking = async (req, res) => {
+  const {
+    date,
+    guests,
+    time,
+    name,
+    email,
+    phones,
+    confirmation,
+  } = req.body.booking;
+  const id = req.body.booking._id;
+  //CHECK IF AVAILABLE
+  let table = Math.ceil(guests / 6);
 
-const {date, guests, time, name, email, phones, confirmation} = req.body.booking
-const id = req.body.booking._id
-//CHECK IF AVAILABLE
-let table = Math.ceil(guests / 6);
+  const bookings = await Booking.find({
+    date: date,
+    time: time,
+  });
 
-const bookings = await Booking.find({
-  date: date,
-  time: time,
-});
+  let totalTables = 0;
+  bookings.map(function (booking) {
+    let bookedTables = Math.ceil(booking.guests / 6);
+    totalTables = totalTables + bookedTables;
+  });
 
-let totalTables = 0;
-bookings.map(function (booking) {
-  let bookedTables = Math.ceil(booking.guests / 6);
-  totalTables = (totalTables + bookedTables);
-});
+  // TABLE IS AVAILABLE OR NOT
+  let isAvailable = false;
+  if (totalTables + table <= 15) {
+    isAvailable = true;
+  } else {
+    isAvailable = false;
+  }
 
-// TABLE IS AVAILABLE OR NOT
-let isAvailable = false;
-if (totalTables + table <= 15) {
-  isAvailable = true;
-} else {
-  isAvailable = false;
-}
+  if (isAvailable == false) {
+    return res.status(400).json({ message: "Bokning tyvärr inte ledigt" });
+  }
 
-if (isAvailable == false) {
-  return res
-  .status(400)
-  .json({ message: "Bokning tyvärr inte ledigt" });
-}
-
-await Booking.findByIdAndUpdate(id, {
-  date: date,
-  guests: guests,
-  time: time
-});
- res.status(200).send()
+  await Booking.findByIdAndUpdate(id, {
+    date: date,
+    guests: guests,
+    time: time,
+  });
+  res.status(200).send();
 };
 
 module.exports = { getBooking, updateBooking };
--- client/src/components/navigation/navigation.tsx
+++ client/src/components/navigation/navigation.tsx
@@ -4,7 +4,6 @@ import { useLocation } from 'react-router-dom';
 
 export const Navigation = () => {
   const location = useLocation();
-console.log(location.pathname);
   
   return (
     <>
